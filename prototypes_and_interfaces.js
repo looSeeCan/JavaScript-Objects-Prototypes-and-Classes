f6a69e630d24c0b4af966caa85e67f90e912826b
looSeeCan <95648904+looSeeCan@users.noreply.github.com>
2022-04-11 15:31:16 -0500

    Leaving office

--- prototypes_and_interfaces.js
+++ prototypes_and_interfaces.js
@@ -36,5 +36,37 @@
     console.log("create a new person from the constructor object")
     let lucycan = new Person2("Lucycan","Ly"); console.log(lucycan);
     console.log(lucycan.__proto__);
-    console.log(lucycan.__proto__ === Person2.prototype);// these two objects are the same instance    
+    console.log(lucycan.__proto__ === Person2.prototype);// these two objects are the same instance. They are the same object in memory. I did not understand this befroe I abandoned the "display" thing. I thought that there was someting going on with that that was making the objects change.
+    //So the object instance that is the functions prototype becomes the prototype for all objects created from that prototype
+    //So, its just the prototypes that are the same. Not the new Object right?
+    console.log("create new person"); let sheng = new Person2("Sheng", "Yang"); console.log(sheng);
+    console.log("is lucycan and sheng the same object:", lucycan === sheng); console.log(`they are not, but thier prototypes are`, sheng.__proto__ === lucycan.__proto__)
+    
 })();
+
+(() => {
+    //create a constructor function
+    // const Person = (firstName, lastName) = {
+    //     this.firstName = firstNme;
+    //     this.lastName = lastName;
+    // };
+
+    function Person (firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName; 
+    };
+
+    let lucycan = new Person("Lucycan", "Ly"); console.log(lucycan);
+    let sheng = new Person("Sheng", "Yang"); console.log(sheng);
+    console.log(lucycan === sheng); console.log(lucycan.__proto__ === sheng.__proto__);
+    //all the same object, because they are all from the same prototype
+    console.log(Person.prototype); console.log(sheng.__proto__); console.log(lucycan.__proto__);
+    //if you change the prototype, the others will change
+    Person.prototype.gender = "female";
+    console.log(Person.prototype.gender); console.log(sheng.gender); console.log(lucycan.gender);
+    lucycan.gender = "male"; console.log(lucycan.gender); console.log(Person.prototype.gender)// but if you change the person the others do not change.
+
+    // sheng.gender = "male";
+    
+})();
+
